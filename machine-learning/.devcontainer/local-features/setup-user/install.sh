740ad9e75943eef5b8baaf202e88ba490ad56772
Chris Brown <Chris.Brown@rhad.us>
2023-11-08 23:40:57 +0000

    Init

---
+++ machine-learning/.devcontainer/local-features/setup-user/install.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+USERNAME=${USERNAME:-"vscode"}
+
+set -eux
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+rm -f /etc/profile.d/00-restore-env.sh
+echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
+chmod +x /etc/profile.d/00-restore-env.sh
+
+export DEBIAN_FRONTEND=noninteractive
+
+HOME_DIR="/home/${USERNAME}/"
+chown -R ${USERNAME}:${USERNAME} ${HOME_DIR}
+chmod -R g+r+w "${HOME_DIR}"
+find "${HOME_DIR}" -type d | xargs -n 1 chmod g+s
+
+OPT_DIR="/opt/"
+chown -R ${USERNAME}:${USERNAME} ${OPT_DIR}
+chmod -R g+r+w "${OPT_DIR}"
+find "${OPT_DIR}" -type d | xargs -n 1 chmod g+s
+
+echo "Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/share:/home/${USERNAME}/.local/bin:${PATH}\"" >> /etc/sudoers.d/$USERNAME
+
+echo "Done!"
